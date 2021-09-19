858eaf5a2d4b7a914ae7c8b32b9f8481b9c3fc95
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-19 10:49:19 -0900

    Update minsh.v

--- minsh.v
+++ minsh.v
@@ -1,8 +1,7 @@
 import os
-mut com := ""
-mut com_ := []
+
 fn main() {
-    com = os.input(">> ")
-    com_ = com.split(" ")
+    com := os.input(">> ")
+    com_ := com.split(" ")
     println(com_[0])
 }
