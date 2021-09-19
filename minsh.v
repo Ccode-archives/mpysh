501511a447b4010e4dd862d4818894e5f529039e
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-19 11:11:26 -0900

    Update minsh.v

--- minsh.v
+++ minsh.v
@@ -1,11 +1,25 @@
 import os
 
+fn exec_comm(com, com_, exists) {
+    if exists == true {
+        os.execute(com)
+    } else {
+        println("minsh: " + com_[0] + ": not found")
+    }
+}
+
 fn main() {
     mut com := ""
     mut com_ := ['']
+    mut exists := true
     for {
         com = os.input(">> ")
         com_ = com.split(" ")
-        println(com_[0])
+        exists = exists_in_system_path(com_[0])
+        if com_[0] == "cd" {
+            os.chdir(com_[1]) or { println("minsh") }
+        } else {
+            exec_comm(com, com_, exists)
+        }
     }
 }
