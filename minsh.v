1839213eb7199d8fea404a82acc4db3a64d576cf
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-19 11:24:34 -0900

    Update minsh.v

--- minsh.v
+++ minsh.v
@@ -19,6 +19,8 @@ fn main() {
         exists = os.exists_in_system_path(com_[0])
         if com_[0] == "cd" {
             os.chdir(com_[1]) or { println(com_[1] + " does not exist or is not a directory") }
+        } else if com_[0] == "exit" {
+            exit(0)
         } else {
             exec_comm(com, com_[0], exists)
         }
