ecc2909b35d62ab1dd1a381baa60638023ccb28b
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-21 17:16:38 -0900

    Update minsh.py

--- minsh.py
+++ minsh.py
@@ -30,7 +30,7 @@ while True:
     if not exists == False:
         exists == True
     if com_[0] == "cd":
-        change = com[0].replace("~", home)
+        change = com[1].replace("~", home)
         try:
             os.chdir(change)
         except:
