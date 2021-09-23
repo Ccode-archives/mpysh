7a9ebced7280b9b254d019bd661be71865eac7ee
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-22 16:43:27 -0900

    add comments

--- minsh.py
+++ minsh.py
@@ -1,44 +1,54 @@
 #!/usr/bin/env python3
 import os
 
+# execute commands
 def exec_comm(com, com_, exists):
+    # check for line end and and statements
     split = com.split(';')
     split2 = com.split('&&')
     if len(split) > 1:
-        print("don't use ; in commands.")
+        print("minsh: don't use ; in commands.")
     elif len(split2) > 1:
-        print("don't use && in commands")
+        print("minsh: don't use && in commands.")
+    # exec builtin
     elif com_ == "exec":
         os.system("~/minsh-bin/" + com)
+    # normal command
     elif exists == True:
         os.system(com)
+    # command does not exist
     else:
         print("minsh: " + com_ + ": not found")
 
 
-
+# exists in path helper
 def exists_in_system_path(name):
     from distutils.spawn import find_executable
     return find_executable(name) is not None
-
+# home var
 home = os.getenv("HOME")
 while True:
+    # set prompt
     pwd = os.getcwd()
     prompt = pwd
     if pwd == home:
         prompt = "~"
     com = input(prompt + " $ ")
+    # misc
     com_ = com.split(" ")
     exists = exists_in_system_path(com_[0])
     if not exists == False:
         exists == True
+    # cd command
     if com_[0] == "cd":
         change = com_[1].replace("~", home)
         try:
             os.chdir(change)
         except:
             print(com_[1] + " does not exist or is not a directory")
+    # exit command
     elif com_[0] == "exit":
         break
     else:
+    # execute command
         exec_comm(com, com_[0], exists)
